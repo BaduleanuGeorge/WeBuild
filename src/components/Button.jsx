1a50f36d5571dea485d1050d6670da715b9ab943
George Baduleanu <baduleanug@gmail.com>
2023-12-10 20:46:38 +0000

    update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Button(props) {
   return (
     <div>
+      {/* Universal button*/}
       <button className="h-[50px] w-[150px] bg-[#f85e00] rounded-sm hover:bg-[#f85e00]/70 duration-300">
         {props.text}
       </button>
